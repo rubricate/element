fcb63c2784696e5274fa381c992f6af44c9e134a
Estefanio NS <estefanions@gmail.com>
2017-09-06 06:22:45 -0300

    interface AddInner Text|Join

--- Rubricate/Element/IAddInnerJoinElement.php
+++ Rubricate/Element/IAddInnerJoinElement.php
@@ -1,18 +1,18 @@
-<?php 
+<?php
 
 /*
  * @package     RubricatePHP
  * @author      Estefanio NS <estefanions AT gmail DOT com>
  * @link        https://github.com/rubricate/element 
- * @copyright   2017 
+ * @copyright   2014 - 2017 
  */
 
 namespace Rubricate\Element;
 
-interface IElement extends 
-    IGetElement, ISetAttributeElement,
-    IAddInnerTextElement, IAddInnerJoinElement
-{
 
+interface IAddInnerJoinElement
+{
+    public function addInnerJoin(IGetElement $innerObj);
 }
+
 
--- Rubricate/Element/IAddInnerTextElement.php
+++ Rubricate/Element/IAddInnerTextElement.php
@@ -1,18 +1,19 @@
-<?php 
+<?php
 
 /*
  * @package     RubricatePHP
  * @author      Estefanio NS <estefanions AT gmail DOT com>
  * @link        https://github.com/rubricate/element 
- * @copyright   2017 
+ * @copyright   2014 - 2017 
  */
 
 namespace Rubricate\Element;
 
-interface IElement extends 
-    IGetElement, ISetAttributeElement,
-    IAddInnerTextElement, IAddInnerJoinElement
-{
 
+interface IAddInnerTextElement
+{
+    public function addInnerText($innerStr);
 }
+
+
 
