8a0bfba3ee82a0b74e7292f9fc7d106a7b832a25
Estefanio N Santos <estefanions@gmail.com>
2021-09-11 22:09:15 -0300

    example

--- example/image.php
+++ example/image.php
@@ -1,16 +1,35 @@
 <?php 
 
 
-require '../vendor/autoload.php';
-
-
 use Rubricate\Element\CreateElement;
 
-$img = new CreateElement('img');
-$img->setAttribute('src', 'image.png');
-echo $img->getElement();
+$i1 = new CreateElement('img');
+$i1->setAttribute('src', 'image.png');
+
+$i2 = new CreateElement('img');
+$i2->setAttribute('src', 'image.png');
+$i2->setAttribute('class', 'img-circle');
+
+$i3 = new CreateElement('img');
+$i3->setAttribute('src', 'image.png');
+$i3->setAttribute('style', 'border: 0');
+
+$a3 = new CreateElement('a');
+$a3->setAttribute('href', '#');
+$a3->addChild($i3);
+
+
+echo $i1->getElement();
+echo $i2->getElement();
+echo $a3->getElement();
+
+
+/* teste */
 
 /*
-    <img src="image.png" />
+<img src="image.png" />
+<img src="image.png" class="img-circle" />
+<a href="#"><img src="image.png" style="border: 0" /></a>
 */
+
 
