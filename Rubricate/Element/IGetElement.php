1b9f01816a2521b5aef0f1a832796b94b96ac1bf
Estefanio N Santos <estefanions@gmail.com>
2023-09-03 23:24:22 -0300

    PSR-12: Extended Coding Style

--- Rubricate/Element/IGetElement.php
+++ Rubricate/Element/IGetElement.php
@@ -1,12 +1,5 @@
 <?php 
 
-/*
- * @package     RubricatePHP
- * @author      Estefanio NS <estefanions AT gmail DOT com>
- * @link        https://github.com/rubricate/element 
- * @copyright   2015 - 2017 
- */
-
 namespace Rubricate\Element;
 
 interface IGetElement
--- Rubricate/Element/StrElement.php
+++ Rubricate/Element/StrElement.php
@@ -1,35 +1,21 @@
 <?php 
 
-/*
- * @package     RubricatePHP
- * @author      Estefanio NS <estefanions AT gmail DOT com>
- * @link        https://github.com/rubricate/element 
- * @copyright   2018
- */
-
 namespace Rubricate\Element;
-
 
 class StrElement implements IGetElement
 {
-    
     private $str;
     private $arg = array();
-
-
 
     public function __construct($str)
     {
         $this->str = $str;
     }
 
-
     public function setParam($arg)
     {
         $this->arg[] = $arg;
     } 
-
-
 
     public function getElement()
     {
@@ -37,8 +23,6 @@ class StrElement implements IGetElement
            (!count($this->arg))? $this->str
            : vsprintf($this->str, $this->arg);
     } 
-
-
 
 }    
 
