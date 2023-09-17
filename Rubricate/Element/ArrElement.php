3c328344de6b9b98c3263904b99cc7d3a6e8ebd8
Estefanio N Santos <estefanions@gmail.com>
2023-09-17 03:06:25 -0300

    exception and return type

--- Rubricate/Element/ArrElement.php
+++ Rubricate/Element/ArrElement.php
@@ -1,24 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Rubricate\Element; 
 
 use ArrayObject;
+use Exception;
 
 class ArrElement implements IArrElement
 {
-    private $attr    = null;
-    private $inner   = null;
-    private $element = null;
+    private $attr, $inner, $element;
+    private $validKey = ['attr', 'inner', 'element'];
 
     public function __construct() {}
 
-    public function get($key)
+    public function get($key): object
     {
-        if(!$this->$key) {
-            $this->$key = new ArrayObject();
+        if(!in_array($key, $this->validKey)){
+
+            throw new Exception("'Only Allowed Key: attr, inner AND element'");
         }
 
-        return $this->$key;
+        return (!$this->$key)? new ArrayObject: $this->$key;
     } 
 
 }    
